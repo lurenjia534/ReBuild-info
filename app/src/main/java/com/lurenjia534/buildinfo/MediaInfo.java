5bbe46b78af58e343dc1ba38037d6abc35b47633
lurenjia534 <lurenjia534@outlook.com>
2024-05-29 14:55:07 +0800

    APP: Update dependencies

---
+++ app/src/main/java/com/lurenjia534/buildinfo/MediaInfo.java
@@ -0,0 +1,10 @@
+package com.lurenjia534.buildinfo;
+
+import android.media.MediaCodec;
+import android.media.MediaCodecList;
+
+public class MediaInfo {
+    public boolean isHDRSupported(String hdrFormat) {
+
+    }
+}
